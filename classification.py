eb594814bed43be2dd60d80218728952a371225d
minaee <sh.minaee@gmail.com>
2020-01-20 19:27:45 +0330

    Linear regression complete

--- classification.py
+++ classification.py
@@ -5,4 +5,45 @@ import numpy as np
 
 df = pd.read_csv("FuelConsumption.csv")
 
-print(df.head())
+# print(df.head())
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+# print(cdf.head(10))
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+# viz.hist()
+# plt.show()
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+# print("train: " + str(len(train)))
+# print("test: " + str(len(test)))
+
+# plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+# plt.xlabel("Engine size")
+# plt.ylabel("Emission")
+# plt.show()
+
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (train_x, train_y)
+# The coefficients
+# print ('Coefficients: ', regr.coef_)
+# print ('Intercept: ',regr.intercept_)
+#
+# plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+# plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+# plt.xlabel("Engine size")
+# plt.ylabel("Emission")
+# plt.show()
+
+from sklearn.metrics import r2_score
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_hat = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y_hat , test_y) )
